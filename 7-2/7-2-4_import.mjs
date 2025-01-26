7825f11f627f3b1305bf4e76d4891ab4a38cc516
hc421020 <nkhshc421020@g.neec.ac.jp>
2025-01-27 03:34:24 +0900

    7-2-4

---
+++ 7-2/7-2-4_import.mjs
@@ -0,0 +1,7 @@
+import {clac1} from "./7-2-4_kake.mjs"
+const result1 = clac1(6, 3)
+console.log("clac1", result1)
+
+import {clac2} from "./7-2-4_wari.mjs"
+const result2 = clac2(6, 3)
+console.log("clac2", result2)
