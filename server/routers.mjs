0feecedff25b8ce52a4f0acd40e7e4b750165258
hc421020 <nkhshc421020@g.neec.ac.jp>
2025-02-10 10:05:23 +0900

    routers

---
+++ server/routers.mjs
@@ -0,0 +1,25 @@
+import express from "express"
+import path from "path"
+import {
+     sample1Controller,
+      sample2Controller
+
+} from "./api/controllers.mjs"
+
+const routers = express.Router()
+
+routers.post("/api/sample1", sample1Controller.postSample1)
+routers.get("/api/sample1", sample1Controller.getSample1)
+
+routers.post("/api/sample2", sample2Controller.postSample2)
+
+routers.get("/api/sample2/:id", sample2Controller.getOneSample2)
+routers.get("/api/sample2", sample2Controller.getAllSample2)
+
+routers.put("/api/sample2/:id", sample2Controller.putSample2)
+routers.delete("/api/sample2/:id", sample2Controller.deleteSample2)
+
+// client配下は、ブラウザサイドで実行されるファイル（HTML/JS/CSS/画像など）を配置する
+routers.use(express.static(`${path.resolve()}/client`))
+
+export default routers
