f161f070e2c9754903cec43800aae24384521532
hc421020 <nkhshc421020@g.neec.ac.jp>
2025-02-17 16:11:36 +0900

    6-6-5

---
+++ 6-6/6-6-5.mjs
@@ -0,0 +1,20 @@
+const objectArray6_6_5 = [
+    {
+        key: "A",
+        value: "カレー",
+    },
+    {
+        key: "B",
+        value: "定食",
+    },
+    {
+        key: "C",
+        value: "ラーメン",
+    }
+]
+
+const map6_6_5 = new Map(objectArray6_6_5.map(object => [object.key, object.value]))
+
+for(const [key, value] of map6_6_5){
+    console.log(key, value)
+}
