befce0d73240f53aab17199c9d4f044054c5d659
hc421020 <nkhshc421020@g.neec.ac.jp>
2025-02-17 13:43:08 +0900

    routers

--- server/routers.mjs
+++ server/routers.mjs
@@ -10,12 +10,14 @@ const routers = express.Router()
 
 routers.post("/api/sample1", sample1Controller.postSample1)
 routers.get("/api/sample1", sample1Controller.getSample1)
+routers.get("/api/sample1/:id", sample1Controller.getPathParamSample1)
+routers.put("/api/sample1", sample1Controller.putSample1)
+routers.delete("/api/sample1", sample1Controller.deleteSample1)
+
 
 routers.post("/api/sample2", sample2Controller.postSample2)
-
 routers.get("/api/sample2/:id", sample2Controller.getOneSample2)
 routers.get("/api/sample2", sample2Controller.getAllSample2)
-
 routers.put("/api/sample2/:id", sample2Controller.putSample2)
 routers.delete("/api/sample2/:id", sample2Controller.deleteSample2)
 
