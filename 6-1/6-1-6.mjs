d2292f61621a34c9e443b38499fcfed095d6c014
hc421020 <nkhshc421020@g.neec.ac.jp>
2025-02-17 10:27:41 +0900

    6-1-6

---
+++ 6-1/6-1-6.mjs
@@ -0,0 +1,14 @@
+let a = 10
+
+console.log(a > 11)
+console.log(a > 8)
+
+console.log(a >= 11)
+console.log(a >= 8)
+
+console.log(a < 11)
+console.log(a < 8)
+
+console.log(a <= 11)
+console.log(a <= 8)
+
