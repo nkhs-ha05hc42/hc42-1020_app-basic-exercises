272819f30406da134789ec5a6713a62fe7a7b1e6
hc421020 <nkhshc421020@g.neec.ac.jp>
2025-02-18 22:46:22 +0900

    controllers

--- server/api/controllers.mjs
+++ server/api/controllers.mjs
@@ -7,4 +7,7 @@
    export * from "./8-1/q8_2Controller.mjs"
    export * from "./8-1/q8_3Controller.mjs"
    export * from "./8-1/q8_4Controller.mjs"
-  
+   export * from "./8-2/q8-2-1Controller.mjs"
+   export * from "./8-2/q8-2-2Controller.mjs"
+   export * from "./8-2/q8-2-3Controller.mjs"
+   export * from "./8-2/q8-2-4Controller.mjs"
