b54769cc391fe890b2a2803546aabd95c6e91ce3
hc421020 <nkhshc421020@g.neec.ac.jp>
2025-02-18 21:58:19 +0900

    7-6-3

---
+++ 7-6/7-6-3.mjs
@@ -0,0 +1,16 @@
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const runAsyncProcess = async () => {
+    console.log("start"); 
+
+    await wait(2000); 
+    console.log(`${new Date().toLocaleTimeString()} - ★`);
+
+    await wait(3000); 
+    console.log(`${new Date().toLocaleTimeString()} - ◆`);
+
+    await wait(2000); 
+    console.log(`${new Date().toLocaleTimeString()} - end`);
+};
+
+runAsyncProcess();
