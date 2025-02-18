19841d25c29901ece2dc737687c64aa76b8e0529
hc421020 <nkhshc421020@g.neec.ac.jp>
2025-02-18 22:31:13 +0900

    q8_3Controller

---
+++ server/api/8-1/q8_3Controller.mjs
@@ -0,0 +1,16 @@
+const put8_1_3 = (req, res) => {
+    console.log(req.body)
+    res.send(
+        JSON.stringify({
+            status: "ok",
+            postcode : req.query.postcode,
+        }),
+    )
+}
+
+
+
+
+export const q8_3Controller = {
+    put8_1_3,
+}
